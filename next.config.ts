4e798c6da17a38e137f2a9dae6019f7d69f3da6a
Marko-Ivanovski <markoivanovski.mi@icloud.com>
2025-11-02 20:27:11 +0100

    Added changes in the next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["@napi-rs/canvas"],
+  outputFileTracingIncludes: {
+    "/api/parse": ["./node_modules/@napi-rs/canvas/**"],
+  },
 };
 
 export default nextConfig;
